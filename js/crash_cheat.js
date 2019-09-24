c480149e39f9eee40f54ebdec4e570c3e297c83f
xwltz <61488281@qq.com>
2019-09-24 11:23:13 +0800

    Site updated: 2019-09-24 11:23:10

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -3,12 +3,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/TEP.ico");
+        $('[rel="icon"]').attr('href', "/images/cry.png");
         document.title = ' ╭(°A°`)╮ 页面崩溃啦 ~ ';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
+        $('[rel="icon"]').attr('href', "/images/favicon-32x32-next.png");
         document.title = ' (ฅ>ω<*ฅ) 诶~ 又好了~ ' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
